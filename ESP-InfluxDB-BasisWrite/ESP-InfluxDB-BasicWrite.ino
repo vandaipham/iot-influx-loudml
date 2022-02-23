7c12ba3675e19fa859e868daa0c6e7b21bd034f6
Van Dai Pham <daipham93@gmail.com>
2022-02-23 14:13:27 +0300

    "second summit"

---
+++ ESP-InfluxDB-BasisWrite/ESP-InfluxDB-BasicWrite.ino
@@ -0,0 +1,139 @@
+/**
+ * Basic Write Example code for InfluxDBClient library for Arduino
+ * Data can be immediately seen in a InfluxDB UI: wifi_status measurement
+ * Enter WiFi and InfluxDB parameters below
+ *
+ * Measures signal level of the actually connected WiFi network
+ * This example supports only InfluxDB running from unsecure (http://...)
+ * For secure (https://...) or Influx Cloud 2 use SecureWrite example
+ **/
+
+#if defined(ESP32)
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32"
+#elif defined(ESP8266)
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti wifiMulti;
+#define DEVICE "ESP8266"
+#endif
+
+#include <InfluxDbClient.h>
+
+// Include DHT22 (temperature and humidity)
+#include "DHT.h"
+#define DHTPIN D6
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+// WiFi AP SSID
+#define WIFI_SSID "bonch"
+// WiFi password
+#define WIFI_PASSWORD "999999999"
+
+// InfluxDB  server url. Don't use localhost, always server name or ip address.
+// E.g. http://192.168.1.48:8086 (In InfluxDB 2 UI -> Load Data -> Client Libraries), 
+#define INFLUXDB_URL "http://20.111.9.42:8086"
+// InfluxDB 2 server or cloud API authentication token (Use: InfluxDB UI -> Load Data -> Tokens -> <select token>)
+// #define INFLUXDB_TOKEN "toked-id"
+// InfluxDB 2 organization id (Use: InfluxDB UI -> Settings -> Profile -> <name under tile> )
+// #define INFLUXDB_ORG "-"
+// InfluxDB 2 bucket name (Use: InfluxDB UI -> Load Data -> Buckets)
+// #define INFLUXDB_BUCKET "bucket"
+
+// InfluxDB v1 database name 
+#define INFLUXDB_DB_NAME "IoTDB"
+
+// InfluxDB v1 username vs password
+#define INFLUXDB_USER "iotuser"
+#define INFLUXDB_PASSWORD "IoTSUT0112210"
+
+// InfluxDB client instance
+// InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
+// InfluxDB client instance for InfluxDB 1
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
+
+// Data point
+Point sensor1("wifi_status");
+Point sensor2("DHT22");
+
+void setup() {
+  Serial.begin(115200);
+
+  // Connect WiFi
+  Serial.println("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+
+  // Set InfluxDB 1 authentication params
+  client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DB_NAME, INFLUXDB_USER, INFLUXDB_PASSWORD);
+
+  // Add constant tags - only once
+  sensor1.addTag("device", DEVICE);
+  sensor1.addTag("SSID", WiFi.SSID());
+
+  // Begin DHT sensor
+  dht.begin();
+  sensor2.addTag("device", DEVICE);
+
+  // Check server connection
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+}
+
+void loop() {
+  // Store measured value into point
+  sensor1.clearFields();
+  sensor2.clearFields();
+
+  // Report RSSI of currently connected network
+  sensor1.addField("rssi", WiFi.RSSI());
+
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  // Store measured sensor values into point
+  sensor2.addField("temperature", t);
+  sensor2.addField("humidity", h);
+
+  // Print what are we exactly writing
+  Serial.print("Writing: ");
+  Serial.println(client.pointToLineProtocol(sensor1));
+  Serial.println(client.pointToLineProtocol(sensor2));
+
+  // If no Wifi signal, try to reconnect it
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("Wifi connection lost");
+  }
+
+  // Write point
+  if (!client.writePoint(sensor1)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  if (!client.writePoint(sensor2)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  //Wait 10s
+  Serial.println("Wait 10s");
+  delay(10000);
+}
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# 1. Run docker-compose to create services: loudml, influxdb, chronograf, kapacitor, and telegraf
+## 1. Run docker-compose to create services: loudml, influxdb, chronograf, kapacitor, and telegraf
 
 
-# 2. Open http://localhost:8888/ to create a new database to save the sensor data.
+## 2. Open http://yourIPaddress:8888/ to create a new database to save the sensor data.
 
+## 3. Open http://yourIPaddress:8888/sources/0/admin-influxdb/databases to create a database named "IoTDB"
 
-# 3. Write sensor data to the influxdb
+## 4. Open http://yourIPaddress:8888/sources/0/admin-influxdb/users to create a user.
+
+## 4. Using Arduino IDE or VS Code with arduino extension to edit file ESP-InfluxDB-BasicWrite.ino.
+Change WIFI_SSID and WIFI_PASSWORD, INFLUXDB_URL, INFLUXDB_DB_NAME, INFLUXDB_USER, INFLUXDB_PASSWORD.
